c0c0439a279eabed01e23d4c4507b0dff4ec9f9e
Riza Ozer <rizaoozer@gmail.com>
2023-04-14 07:42:25 +0300

    Tech-6 Employee can search employees with paginated results demo

--- api/src/main/java/com/knits/enterprise/dto/search/EmployeeSearchDto.java
+++ api/src/main/java/com/knits/enterprise/dto/search/EmployeeSearchDto.java
@@ -1,6 +1,10 @@
 package com.knits.enterprise.dto.search;
 
+import com.knits.enterprise.model.common.Country;
+import com.knits.enterprise.model.company.BusinessUnit;
+import com.knits.enterprise.model.company.Department;
 import com.knits.enterprise.model.company.Employee;
+import com.knits.enterprise.model.company.JobTitle;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,11 +12,17 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -23,10 +33,82 @@ import java.util.List;
 @Slf4j
 public class EmployeeSearchDto extends GenericSearchDto<Employee>{
 
+    private String firstName;
     private String lastName;
     private String gender;
 
+    private String hireDateFrom;
+    private String hireDateTo;
+    private Long businessUnitId;
+    private Long departmentId;
+    private Long jobTitleId;
+    private Long countryId;
 
+    @Override
+    public Specification<Employee> getSpecification() {
+        return (root, query, criteriaBuilder) -> {
+
+            query.distinct(true);
+            Predicate noFiltersApplied = criteriaBuilder.conjunction();
+            List<Predicate> filters = new ArrayList<>();
+            filters.add(noFiltersApplied);
+
+            //firstName
+            if(Strings.isNotBlank(firstName)) {
+                Predicate firstNameAsPredicate = criteriaBuilder.equal(root.get(firstName), firstName);
+                filters.add(firstNameAsPredicate);
+            }
+
+            // lastName
+            if(Strings.isNotBlank(lastName)) {
+                Predicate lastNameAsPredicate = criteriaBuilder.equal(root.get(lastName), lastName);
+                filters.add(lastNameAsPredicate);
+            }
+
+            // hireDateFrom
+            if(Strings.isNotBlank(hireDateFrom)) {
+                LocalDate dateFrom = LocalDate.parse(hireDateFrom, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                Predicate hireDateFromAsPredicate = criteriaBuilder.greaterThanOrEqualTo(root.get("startDate"), dateFrom);
+                filters.add(hireDateFromAsPredicate);
+            }
+
+            // hireDateTo
+            if(Strings.isNotBlank(hireDateTo)) {
+                LocalDate dateTo = LocalDate.parse(hireDateTo, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                Predicate hireDateToAsPredicate = criteriaBuilder.lessThanOrEqualTo(root.get("startDate"), dateTo);
+                filters.add(hireDateToAsPredicate);
+            }
+
+
+            // businessUnit
+            if(businessUnitId != null) {
+                Predicate businessUnitIdAsPredicate = criteriaBuilder.equal(root.get("businessUnit").get("id"), businessUnitId);
+                filters.add(businessUnitIdAsPredicate);
+            }
+
+            // department
+            if(departmentId != null) {
+                Predicate departmentIdAsPredicate = criteriaBuilder.equal(root.get("department").get("id"), businessUnitId);
+                filters.add(departmentIdAsPredicate);
+            }
+
+            // jobTitle
+            if(jobTitleId != null) {
+                Predicate jobTitleIdAsPredicate = criteriaBuilder.equal(root.get("jobTitle").get("id"), jobTitleId);
+                filters.add(jobTitleIdAsPredicate);
+            }
+
+            // office
+            if(countryId != null) {
+                Predicate countryIdAsPredicate = criteriaBuilder.equal(root.get("office").get("country").get("id"), countryId);
+                filters.add(countryIdAsPredicate);
+            }
+
+            return criteriaBuilder.and(filters.toArray(new Predicate[filters.size()]));
+    };
+}
 
     protected void addFilters(Root<Employee> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, List<Predicate> filters) {
 
