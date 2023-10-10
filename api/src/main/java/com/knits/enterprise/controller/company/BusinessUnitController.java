d2cb86de6dfc40aca7bb3e58bbd1f4985f75602d
nikasn <nikasn@ttu.ee>
2023-10-10 15:46:36 +0300

    [Tech-1]: implemented Pagination for BusinessUnit.

--- api/src/main/java/com/knits/enterprise/controller/company/BusinessUnitController.java
+++ api/src/main/java/com/knits/enterprise/controller/company/BusinessUnitController.java
@@ -1,12 +1,16 @@
 package com.knits.enterprise.controller.company;
 
+import com.knits.enterprise.dto.common.PaginatedResponseDto;
 import com.knits.enterprise.dto.company.BusinessUnitDto;
 import com.knits.enterprise.dto.company.EmployeeDto;
+import com.knits.enterprise.dto.search.GenericSearchDto;
+import com.knits.enterprise.model.company.BusinessUnit;
 import com.knits.enterprise.service.company.BusinessUnitService;
 import com.knits.enterprise.service.company.EmployeeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,5 +44,11 @@ public class BusinessUnitController {
         return ResponseEntity
                 .ok()
                 .body(businessUnitFound);
+    }
+
+    @GetMapping(value = "/businessUnits", produces = {"application/json"}, consumes = {"application/json"})
+    public PaginatedResponseDto<BusinessUnitDto> getPaginatedContent(@RequestBody GenericSearchDto<BusinessUnit> searchDto){
+        PaginatedResponseDto<BusinessUnitDto> paginatedResponseDto = businessUnitService.listAll(searchDto);
+        return paginatedResponseDto;
     }
 }
