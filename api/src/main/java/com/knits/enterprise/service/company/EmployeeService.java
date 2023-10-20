f41b912e1c54ec56bb7836c03faa3140b49a6470
Nikita Kašnikov <nikasn@taltech.ee>
2023-10-20 09:58:24 +0300

    [Tech-7]: refactor deleted id column from exel

--- api/src/main/java/com/knits/enterprise/service/company/EmployeeService.java
+++ api/src/main/java/com/knits/enterprise/service/company/EmployeeService.java
@@ -110,59 +110,56 @@ public class EmployeeService {
         if (listEmployeesDtos.isEmpty()) return;
         Row row = sheet.createRow(0);
         row.createCell(0).setCellValue(
-                creationHelper.createRichTextString("id"));
-        row.createCell(1).setCellValue(
                 creationHelper.createRichTextString("firstName"));
-        row.createCell(2).setCellValue(
+        row.createCell(1).setCellValue(
                 creationHelper.createRichTextString("lastname"));
-        row.createCell(3).setCellValue(
+        row.createCell(2).setCellValue(
                 creationHelper.createRichTextString("email"));
-        row.createCell(4).setCellValue(
+        row.createCell(3).setCellValue(
                 creationHelper.createRichTextString("birthDate"));
-        row.createCell(5).setCellValue(
+        row.createCell(4).setCellValue(
                 creationHelper.createRichTextString("gender"));
-        row.createCell(6).setCellValue(
+        row.createCell(5).setCellValue(
                 creationHelper.createRichTextString("startDate"));
-        row.createCell(7).setCellValue(
+        row.createCell(6).setCellValue(
                 creationHelper.createRichTextString("endDate"));
-        row.createCell(8).setCellValue(
+        row.createCell(7).setCellValue(
                 creationHelper.createRichTextString("companyPhone"));
-        row.createCell(9).setCellValue(
+        row.createCell(8).setCellValue(
                 creationHelper.createRichTextString("role"));
-        row.createCell(10).setCellValue(
+        row.createCell(9).setCellValue(
                 creationHelper.createRichTextString("businessUnit"));
-        row.createCell(11).setCellValue(
+        row.createCell(10).setCellValue(
                 creationHelper.createRichTextString("organization"));
-        row.createCell(12).setCellValue(
+        row.createCell(11).setCellValue(
                 creationHelper.createRichTextString("office"));
-        row.createCell(13).setCellValue(
+        row.createCell(12).setCellValue(
                 creationHelper.createRichTextString("jobTitle"));
-        row.createCell(14).setCellValue(
+        row.createCell(13).setCellValue(
                 creationHelper.createRichTextString("department"));
-        row.createCell(15).setCellValue(
+        row.createCell(14).setCellValue(
                 creationHelper.createRichTextString("division"));
-        row.createCell(16).setCellValue(
+        row.createCell(15).setCellValue(
                 creationHelper.createRichTextString("solidLineManager"));
         for (int i = 0; i < listEmployeesDtos.size(); i++) {
             row = sheet.createRow(i + 1);
             EmployeeDto employeeDto = listEmployeesDtos.get(i);
-            row.createCell(0).setCellValue(employeeDto.getId());
-            row.createCell(1).setCellValue(employeeDto.getFirstName() != null ? employeeDto.getFirstName().toString() : null);
-            row.createCell(2).setCellValue(employeeDto.getLastName() != null ? employeeDto.getLastName().toString() : null);
-            row.createCell(3).setCellValue(employeeDto.getEmail() != null ? employeeDto.getEmail().toString() : null);
-            row.createCell(4).setCellValue(employeeDto.getBirthDate() != null ? employeeDto.getBirthDate().toString() : null);
-            row.createCell(5).setCellValue(employeeDto.getGender() != null ? employeeDto.getGender().toString() : null);
-            row.createCell(6).setCellValue(employeeDto.getStartDate() != null ? employeeDto.getStartDate().toString() : null);
-            row.createCell(7).setCellValue(employeeDto.getEndDate() != null ? employeeDto.getEndDate().toString() : null);
-            row.createCell(8).setCellValue(employeeDto.getCompanyPhone() != null ? employeeDto.getCompanyPhone().toString() : null);
-            row.createCell(9).setCellValue(employeeDto.getRole() != null ? employeeDto.getRole().toString() : null);
-            row.createCell(10).setCellValue(employeeDto.getBusinessUnit() != null ? employeeDto.getBusinessUnit().toString() : null);
-            row.createCell(11).setCellValue(employeeDto.getOrganization() != null ? employeeDto.getOrganization().toString() : null);
-            row.createCell(12).setCellValue(employeeDto.getOffice() != null ? employeeDto.getOffice().toString() : null);
-            row.createCell(13).setCellValue(employeeDto.getJobTitle() != null ? employeeDto.getJobTitle().toString() : null);
-            row.createCell(14).setCellValue(employeeDto.getDepartment() != null ? employeeDto.getDepartment().toString() : null);
-            row.createCell(15).setCellValue(employeeDto.getDivision() != null ? employeeDto.getDivision().toString() : null);
-            row.createCell(16).setCellValue(employeeDto.getSolidLineManager() != null ? employeeDto.getSolidLineManager().toString() : null);
+            row.createCell(0).setCellValue(employeeDto.getFirstName() != null ? employeeDto.getFirstName().toString() : null);
+            row.createCell(1).setCellValue(employeeDto.getLastName() != null ? employeeDto.getLastName().toString() : null);
+            row.createCell(2).setCellValue(employeeDto.getEmail() != null ? employeeDto.getEmail().toString() : null);
+            row.createCell(3).setCellValue(employeeDto.getBirthDate() != null ? employeeDto.getBirthDate().toString() : null);
+            row.createCell(4).setCellValue(employeeDto.getGender() != null ? employeeDto.getGender().toString() : null);
+            row.createCell(5).setCellValue(employeeDto.getStartDate() != null ? employeeDto.getStartDate().toString() : null);
+            row.createCell(6).setCellValue(employeeDto.getEndDate() != null ? employeeDto.getEndDate().toString() : null);
+            row.createCell(7).setCellValue(employeeDto.getCompanyPhone() != null ? employeeDto.getCompanyPhone().toString() : null);
+            row.createCell(8).setCellValue(employeeDto.getRole() != null ? employeeDto.getRole().toString() : null);
+            row.createCell(9).setCellValue(employeeDto.getBusinessUnit() != null ? employeeDto.getBusinessUnit().toString() : null);
+            row.createCell(10).setCellValue(employeeDto.getOrganization() != null ? employeeDto.getOrganization().toString() : null);
+            row.createCell(11).setCellValue(employeeDto.getOffice() != null ? employeeDto.getOffice().toString() : null);
+            row.createCell(12).setCellValue(employeeDto.getJobTitle() != null ? employeeDto.getJobTitle().toString() : null);
+            row.createCell(13).setCellValue(employeeDto.getDepartment() != null ? employeeDto.getDepartment().toString() : null);
+            row.createCell(14).setCellValue(employeeDto.getDivision() != null ? employeeDto.getDivision().toString() : null);
+            row.createCell(15).setCellValue(employeeDto.getSolidLineManager() != null ? employeeDto.getSolidLineManager().toString() : null);
 
         }
         // Write the output to a file
