d548b2ee5055feedaf524a32aa3ae0db5ec2d3fa
Nikita Kašnikov <nikasn@taltech.ee>
2024-02-27 23:15:13 +0200

    refactor EmployeeController URLs and some return types

--- src/main/java/com/knits/enterprise/controller/company/EmployeeController.java
+++ src/main/java/com/knits/enterprise/controller/company/EmployeeController.java
@@ -7,20 +7,24 @@ import com.knits.enterprise.service.company.ContractService;
 import com.knits.enterprise.service.company.EmployeeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.core.io.Resource;
 
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
+import static org.springframework.http.MediaType.*;
+
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/employee-service/employees")
 @Slf4j
 public class EmployeeController {
 
@@ -29,7 +33,7 @@ public class EmployeeController {
 
     private final ContractService contractService;
 
-    @PostMapping(value = "/employees", produces = {"application/json"}, consumes = {"application/json"})
+    @PostMapping(produces = {"application/json"}, consumes = {"application/json"})
     public ResponseEntity<EmployeeDto> createNewEmployee(@RequestBody EmployeeDto employeeDto) {
         log.debug("REST request to create Employee");
         return ResponseEntity
@@ -37,7 +41,7 @@ public class EmployeeController {
                 .body(employeeService.saveNewEmployee(employeeDto));
     }
 
-    @GetMapping(value = "/employees/{id}", produces = {"application/json"})
+    @GetMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable(value = "id") final Long id) {
         log.debug("REST request to get Employee : {}", id);
         EmployeeDto employeeFound = employeeService.findEmployeeById(id);
@@ -47,7 +51,7 @@ public class EmployeeController {
 
     }
 
-    @PatchMapping(value = "/employees", produces = {"application/json"}, consumes = {"application/json"})
+    @PatchMapping(produces = {"application/json"}, consumes = {"application/json"})
     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable(value = "id") @RequestBody EmployeeDto employeeDto) {
         EmployeeDto employeeFound = employeeService.partialUpdate(employeeDto);
         return ResponseEntity
@@ -55,7 +59,7 @@ public class EmployeeController {
                 .body(employeeFound);
     }
 
-    @PutMapping(value = "/employees", produces = {"application/json"})
+    @PutMapping(produces = {"application/json"})
     public ResponseEntity<EmployeeDto> deleteEmployee(@PathVariable(value = "id") final Long id) {
         log.debug("REST request to delete Employee : {}", id);
         EmployeeDto employeeFound = employeeService.deleteEmployee(id);
@@ -64,7 +68,7 @@ public class EmployeeController {
                 .body(employeeFound);
     }
 
-    @GetMapping(value = "/employees", produces = {"application/json"})
+    @GetMapping(produces = {"application/json"})
     public ResponseEntity<PageImpl<EmployeeDto>> searchForEmployees(@RequestBody(required = false) EmployeeSearchDto searchDto){
         if(searchDto == null){
             searchDto = new EmployeeSearchDto();
@@ -75,19 +79,20 @@ public class EmployeeController {
                 .body(foundedEmployeeDtos);
     }
 
-    @GetMapping(value = "/employees/export/excel", produces = {"application/json"})
-    public ResponseEntity excelDocumentForEmployees(@RequestBody(required = false) EmployeeSearchDto searchDto){
+    @GetMapping(value = "/excel", produces = APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<Resource> downloadExcelReportForEmployees(@RequestBody(required = false) EmployeeSearchDto searchDto){
         if(searchDto == null){
             searchDto = new EmployeeSearchDto();
         }
         ByteArrayOutputStream report = employeeService.makeExelOfEmployees(searchDto);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Disposition", "attachment; filename=report.xlsx");
-        return ResponseEntity.ok().headers(headers).body(report.toByteArray());
+        ByteArrayResource resource = new ByteArrayResource(report.toByteArray());
+        return ResponseEntity.ok().headers(headers).body(resource);
     }
 
-    @PostMapping(value = "/employees/import/excel")
-    public ResponseEntity addEmployeesFromExcelFile(@RequestParam(value = "fileName") String fileName){
+    @PostMapping(value = "/excel")
+    public ResponseEntity<Void> createEmployeesFromExcelFile(@RequestParam(value = "fileName") String fileName){
         if(fileName == null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
@@ -95,22 +100,22 @@ public class EmployeeController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping(value = "/employees/analysis", produces = {"application/json"})
+    @GetMapping(value = "/analysis", produces = {"application/json"})
     public ResponseEntity getEmployeesAnalysis(){
         EmployeeAnalyticsDto employeeAnalyticsDto = employeeService.employeeAnalytics();
         return ResponseEntity.ok().body(employeeAnalyticsDto);
     }
 
-    @GetMapping(value = "/employees/contracts/all", produces = {"application/json"})
-    public ResponseEntity<List<Long>> findAllContracts() {
+    @GetMapping(value = "/contracts/ids", produces = {"application/json"})
+    public ResponseEntity<List<Long>> findAllContractsIds() {
         return ResponseEntity.ok(contractService.findContractsIds());
     }
-    @GetMapping("/employees/contracts")
+    @GetMapping("/contracts/zip")
     public ResponseEntity getEmployeeContracts(@RequestParam List<Long> ids) {
         byte[] bytes = contractService.makeContractsZipFileByIds(ids);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentDispositionFormData("attachment", "employees.zip");
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentType(APPLICATION_OCTET_STREAM);
         return ResponseEntity.ok().headers(headers).body(bytes);
     }
 
