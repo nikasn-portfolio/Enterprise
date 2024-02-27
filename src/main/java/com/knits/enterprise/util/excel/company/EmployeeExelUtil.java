c327512e05f36e4106976804d3cb5ca01908e035
Nikita Kašnikov <nikasn@taltech.ee>
2024-02-28 01:06:58 +0200

    refactor access modifaers on constants

--- src/main/java/com/knits/enterprise/util/excel/company/EmployeeExelUtil.java
+++ src/main/java/com/knits/enterprise/util/excel/company/EmployeeExelUtil.java
@@ -112,25 +112,21 @@ public class EmployeeExelUtil {
         return bos;
     }
     protected static class ExcelEmployeeTitleKey{
-        protected static final String FIRST_NAME_KEY = "firstName";
-        protected static final String LAST_NAME_KEY = "lastName";
-        protected static final String EMAIL_KEY = "email";
-        protected static final String BIRTH_DATE_KEY = "birthDate";
-        protected static final String GENDER_KEY = "gender";
-
-        protected static final String START_DATE_KEY = "startDate";
-        protected static final String END_DATE_KEY = "endDate";
-
-        protected static final String COMPANY_PHONE_KEY = "companyPhone";
-
-        protected static final String ROLE_KEY = "role";
-        protected static final String BUSINESS_UNIT_KEY = "businessUnit";
-
-        protected static final String ORGANIZATION_KEY = "organization";
-        protected static final String OFFICE_KEY = "office";
-        protected static final String JOB_TITLE_KEY = "jobTitle";
-        protected static final String DEPARTMENT_KEY = "department";
-        protected static final String DIVISION_KEY = "division";
-        protected static final String SOLID_LINE_MANAGER_KEY = "solidLineManager";
+        public static final String FIRST_NAME_KEY = "firstName";
+        public static final String LAST_NAME_KEY = "lastName";
+        public static final String EMAIL_KEY = "email";
+        public static final String BIRTH_DATE_KEY = "birthDate";
+        public static final String GENDER_KEY = "gender";
+        public static final String START_DATE_KEY = "startDate";
+        public static final String END_DATE_KEY = "endDate";
+        public static final String COMPANY_PHONE_KEY = "companyPhone";
+        public static final String ROLE_KEY = "role";
+        public static final String BUSINESS_UNIT_KEY = "businessUnit";
+        public static final String ORGANIZATION_KEY = "organization";
+        public static final String OFFICE_KEY = "office";
+        public static final String JOB_TITLE_KEY = "jobTitle";
+        public static final String DEPARTMENT_KEY = "department";
+        public static final String DIVISION_KEY = "division";
+        public static final String SOLID_LINE_MANAGER_KEY = "solidLineManager";
     }
 }
